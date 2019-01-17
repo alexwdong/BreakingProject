127dc6a6db47566ea1f6416576c43da475313fac
Alex Dong <alex.w.dong@gmail.com>
2019-01-17 00:17:59 -0500

    added preliminary draft for Judge_results_for_bboys, and added parse_bboy_to_nationality, as well as created a data file storing bboys to their nationality

---
+++ Code/parse_bboy_to_nationality.py
@@ -0,0 +1,24 @@
+import re
+import warnings
+
+def parse_file_to_dict(txt_file_str):
+  '''
+  Parses a two column semicolon delimited file into a dictionary
+
+  Comments in the file can be specified by using the # character
+  '''
+  return_dict = {}
+  pattern = re.compile('(?P<name>.*) ; (?<nationality>.*)',re.M)
+  with open(txt_file_str) as file:
+    line_number = 0
+    for line in file:
+      line_number = line_number+1
+      line.replace("#.","") # removes comments
+      match_obj = re.fullmatch(pattern,line)
+      if match_obj is None:
+        warnings.warn("In file "+ txt_file_str + " on line " + line_number + " no match was found")
+      
+      else:
+        return_dict[match_obj["name"]] = match_obj["nationality"]
+return return_dict 
+
